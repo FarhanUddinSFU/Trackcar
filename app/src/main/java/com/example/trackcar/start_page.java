78821280bf9452ef19f6819ed13ced38f90f7c0e
FARHANUDDINSFU <farhan.uddin08@outlook.com>
2025-09-03 13:49:43 -0700

    Initial commit

---
+++ app/src/main/java/com/example/trackcar/start_page.java
@@ -0,0 +1,31 @@
+package com.example.trackcar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class start_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_start_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    //start_page -> my_garage
+    public void myGarageClicked(View V) {
+        Intent i = new Intent(this, my_garage.class);
+        startActivity(i);
+    }
+}
